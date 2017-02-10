102b0cdbd50a05fbb6992c48821a56ed1ff244c3
Kenneth Korcal <kakorcal@gmail.com>
2017-02-09 17:15:10 -0800

    completed reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
 // the MobileMenu is just a variable it can be named anything
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($('.feature-item'), '85%'); 
+new RevealOnScroll($('.testimonial'), '60%'); 
