a6efb15171d3298bfeb04311b4dd4488b0092af0
Kenneth Korcal <kakorcal@gmail.com>
2017-02-09 19:53:13 -0800

    complete sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 // the MobileMenu is just a variable it can be named anything
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%'); 
 new RevealOnScroll($('.testimonial'), '60%'); 
+var stickyHeader = new StickyHeader();
